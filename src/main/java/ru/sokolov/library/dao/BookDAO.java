49964094a212d3dc8842a001734839809512e87a
unhoJly <sokolov_it@mail.ru>
2023-04-27 02:29:43 +0300

    27.04.23

---
+++ src/main/java/ru/sokolov/library/dao/BookDAO.java
@@ -0,0 +1,65 @@
+package ru.sokolov.library.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import ru.sokolov.library.models.Book;
+import ru.sokolov.library.models.Person;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class BookDAO {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> index() {
+        return jdbcTemplate.query("SELECT * FROM book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book show(int id) {
+        return jdbcTemplate
+                .query("SELECT * FROM book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO book (title, author, year) VALUES(?, ?, ?)",
+                book.getTitle(), book.getAuthor(), book.getYear());
+    }
+
+    public void update(int id, Book updatedBook) {
+        jdbcTemplate.update("UPDATE book SET title=?, author=?, year=? WHERE id=?",
+                updatedBook.getTitle(), updatedBook.getAuthor(), updatedBook.getYear(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM book WHERE id=?", id);
+    }
+
+    // Join'им таблицы Book и Person и получаем человека, которому принадлежит книга с указанным id
+    public Optional<Person> getBookOwner(int id) {
+        // Выбираем все колонки таблицы person из объединённой таблицы
+        return jdbcTemplate
+                .query("SELECT person.* FROM book JOIN person ON book.person_id = person.id WHERE book.id = ?",
+                        new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+                .stream().findAny();
+    }
+
+    // Освобождает книгу (этот метод вызывается, когда человек возвращает книгу в библиотеку)
+    public void release(int id) {
+        jdbcTemplate.update("UPDATE book SET person_id=NULL WHERE id=?", id);
+    }
+
+    // Назначает книгу человеку (этот метод вызывается, когда человек забирает книгу из библиотеки)
+    public void assign(int id, Person selectedPerson) {
+        jdbcTemplate.update("UPDATE book SET person_id=? WHERE id=?", selectedPerson.getId(), id);
+    }
+}
