8115db1d4ca89fdd88e1f41ae2138e99179dc7f5
unhoJly <sokolov_it@mail.ru>
2023-05-02 00:31:08 +0300

    02.05.23

--- src/main/java/ru/sokolov/library/dao/BookDAO.java
+++ src/main/java/ru/sokolov/library/dao/BookDAO.java
@@ -25,23 +25,26 @@ public class BookDAO {
     }
 
     public Book show(int id) {
-        return jdbcTemplate
-                .query("SELECT * FROM book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
-                .stream().findAny().orElse(null);
+//        return jdbcTemplate
+//                .query("SELECT * FROM book WHERE id = ?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+//                .stream().findAny().orElse(null);
+
+        return jdbcTemplate.queryForObject("SELECT * FROM book WHERE id=?", new Object[]{id},
+                new BeanPropertyRowMapper<>(Book.class));
     }
 
     public void save(Book book) {
-        jdbcTemplate.update("INSERT INTO book (title, author, year) VALUES(?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO book (title, author, year) VALUES (?, ?, ?)",
                 book.getTitle(), book.getAuthor(), book.getYear());
     }
 
     public void update(int id, Book updatedBook) {
-        jdbcTemplate.update("UPDATE book SET title=?, author=?, year=? WHERE id=?",
+        jdbcTemplate.update("UPDATE book SET title = ?, author = ?, year = ? WHERE id = ?",
                 updatedBook.getTitle(), updatedBook.getAuthor(), updatedBook.getYear(), id);
     }
 
     public void delete(int id) {
-        jdbcTemplate.update("DELETE FROM book WHERE id=?", id);
+        jdbcTemplate.update("DELETE FROM book WHERE id = ?", id);
     }
 
     // Join'им таблицы Book и Person и получаем человека, которому принадлежит книга с указанным id
@@ -51,15 +54,19 @@ public class BookDAO {
                 .query("SELECT person.* FROM book JOIN person ON book.person_id = person.id WHERE book.id = ?",
                         new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny();
+//    public Person getBookOwner(int id) {
+//        return jdbcTemplate
+//        .queryForObject("SELECT person.* FROM book JOIN person ON book.person_id = person.id WHERE book.id = ?",
+//                        new Object[]{id}, new BeanPropertyRowMapper<>(Person.class));
     }
 
     // Освобождает книгу (этот метод вызывается, когда человек возвращает книгу в библиотеку)
     public void release(int id) {
-        jdbcTemplate.update("UPDATE book SET person_id=NULL WHERE id=?", id);
+        jdbcTemplate.update("UPDATE book SET person_id = NULL WHERE id = ?", id);
     }
 
     // Назначает книгу человеку (этот метод вызывается, когда человек забирает книгу из библиотеки)
     public void assign(int id, Person selectedPerson) {
-        jdbcTemplate.update("UPDATE book SET person_id=? WHERE id=?", selectedPerson.getId(), id);
+        jdbcTemplate.update("UPDATE book SET person_id = ? WHERE id = ?", selectedPerson.getId(), id);
     }
 }
